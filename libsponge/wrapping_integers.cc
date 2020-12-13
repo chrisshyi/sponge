95a2ec3f58f3a5fef96f6bbd027437b8d8f3aae1
Chris Shyi <chrisshyi13@gmail.com>
2020-12-13 13:54:13 +0800

    Finish lab2 part 1: wrapping integers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t isn_u64 = static_cast<uint64_t>(isn.raw_value());
+    uint64_t seq_no_u64 = (n + isn_u64) % (static_cast<uint64_t>(1) << 32);
+    return WrappingInt32{static_cast<uint32_t>(seq_no_u64)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +30,24 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t n_64 = static_cast<uint64_t>(n.raw_value());
+    uint64_t isn_64 = static_cast<uint64_t>(isn.raw_value());
+    
+    uint64_t u32 = static_cast<uint64_t>(1) << 32;
+    
+    uint64_t init_diff;
+    if (n_64 >= isn_64) {
+        init_diff = n_64 - isn_64;
+    } else {
+        init_diff = u32 - isn_64 + n_64;
+    }
+    if (checkpoint <= init_diff) {
+        return init_diff;
+    }
+    uint64_t multiplier = (checkpoint - init_diff) / u32;
+    uint64_t low_num = init_diff + u32 * multiplier;
+    uint64_t high_num = init_diff + u32 * (multiplier + 1);
+    
+    uint64_t low_diff = checkpoint - low_num, high_diff = high_num - checkpoint;
+    return low_diff < high_diff ? low_num : high_num;
 }
