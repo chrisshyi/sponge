c420de0280a3b197b1a77e6a7f44bd4cb9e2b9c3
Chris Shyi <chrisshyi13@gmail.com>
2021-01-15 21:31:22 +0800

    Update webget with custom socket

--- apps/webget.cc
+++ apps/webget.cc
@@ -18,7 +18,7 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
     const Address addr{host, "http"};
-    CS144TCPSocket sock{};
+    FullStackSocket sock{};
     sock.connect(addr);
     std::stringstream ss;
     ss << "GET " << path << " HTTP/1.1\r\n";
